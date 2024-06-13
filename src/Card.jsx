9b3c4bdc65f9dbff3870c441911238fd428ecd75
Vitor Kamenschek <vitorkamenschek@gmail.com>
2024-06-13 14:56:27 -0300

    mudança de url

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,7 +4,7 @@ function Card () {
             <img className="card-image" src="https://media.licdn.com/dms/image/D4D03AQHADyICE2UEUw/profile-displayphoto-shrink_200_200/0/1709776354471?e=1723680000&v=beta&t=2DLhZnwoLmeFpeGiALWMWn4MM8V6r2FnmBn3BXGH4HA" alt="profile picture" />
             <h2 className="card-title">Vitor Kamenschek</h2>
             <p className="card-description">Estudante de ADS e Desenvolvedor Web</p>
-            <a href="https://www.linkedin.com/in/vitor-kamenschek/" target="_blank" rel="noopener noreferrer" className="card-button">Veja meu LinkedIn</a>
+            <a href="https://www.linkedin.com/in/v%C3%ADtor-kamenschek/" target="_blank" rel="noopener noreferrer" className="card-button">Veja meu LinkedIn</a>
         </div>
     )
 }
